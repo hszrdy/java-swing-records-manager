c1a7237c9ac25ef796b2167f9c16ca0b5764a359
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2023-05-23 02:38:37 +0800

    Create RecordsView.java
    GUI for the records window

---
+++ src/mp5/RecordsView.java
@@ -0,0 +1,80 @@
+package mp5;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class RecordsView extends JFrame
+{
+    private JTextArea recordArea;
+    private JScrollPane scrollPane;
+    private JComboBox<String> dropMenu;
+    private JRadioButton rAscending, rDescending;
+    private JButton addRecordBtn, removeRecordBtn, exportBtn;
+    private JPanel tblPanel, optionContainer, btnContainer, radioContainer;
+    private ButtonGroup rBtnGroup;
+    private JLabel label;
+    
+    private static final String[] sortOptions = 
+        { "Name", "Birthday", "Age" };
+    
+    public RecordsView()
+    {
+        dropMenu = new JComboBox<>(sortOptions);
+        recordArea = new JTextArea(15,30);
+        rBtnGroup = new ButtonGroup();
+        rAscending = new JRadioButton();
+        rDescending = new JRadioButton();
+        addRecordBtn = new JButton();
+        removeRecordBtn = new JButton();
+        exportBtn = new JButton();
+        scrollPane = new JScrollPane(recordArea);
+        label = new JLabel();
+        optionContainer = new JPanel();
+        tblPanel = new JPanel();
+        btnContainer = new JPanel();
+        radioContainer = new JPanel();
+        
+        init();
+    }
+    
+    public void init()
+    {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 600);
+        this.setTitle("Records");
+        this.setResizable(false);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        
+        recordArea.setEditable(false);
+        rDescending.setText("Descending");
+        rAscending.setText("Ascending");
+        exportBtn.setText("Export to CSV");
+        addRecordBtn.setText("Add record");
+        removeRecordBtn.setText("Remove record");
+        label.setText("Sort By: ");
+        
+        rBtnGroup.add(rAscending);
+        rBtnGroup.add(rDescending);
+        
+        optionContainer.setBackground(Color.red);
+        tblPanel.setBackground(Color.BLUE);
+        
+        tblPanel.add(scrollPane);
+        optionContainer.add(label);
+        optionContainer.add(dropMenu);
+        optionContainer.add(rAscending);
+        optionContainer.add(rDescending);
+        btnContainer.add(addRecordBtn);
+        btnContainer.add(removeRecordBtn);
+        btnContainer.add(exportBtn);
+        
+        
+        this.add(tblPanel);
+        this.add(optionContainer);
+        this.add(btnContainer);
+    }
+}
