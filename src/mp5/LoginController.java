fb5ae162a8d71467ca845e081f19c50abd2e1fe4
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2023-05-23 02:37:16 +0800

    Update LoginController.java
    Login Controller for Model (Credentials) and Viewmodel (LoginView)

--- src/mp5/LoginController.java
+++ src/mp5/LoginController.java
@@ -1,6 +1,53 @@
 package mp5;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class LoginController 
 {
+    LoginView viewmodel;
+    Credentials creds;
+    private boolean isLoggedIn = false;
     
+    public LoginController(LoginView viewmodel, Credentials creds)
+    {
+        this.viewmodel = viewmodel;
+        this.creds = creds;
+        
+        this.viewmodel.addBtnListener(loginButtonListener);
+    }
+    
+    public boolean getLoginStatus()
+    {
+        return isLoggedIn;
+    }
+    
+    MouseAdapter loginButtonListener = new MouseAdapter()
+    {
+        @Override
+        public void mouseClicked(MouseEvent e) 
+        {
+            String u = viewmodel.getUsername();
+            String p = viewmodel.getPassword();
+            
+            if(!creds.checkCredentials(u, p))
+            {
+                viewmodel.displayLoginError();
+                creds.incrementTryCount();
+            }
+            else
+            {
+                System.out.println("Login successful");
+                isLoggedIn = true;
+                viewmodel.closeFrame();
+            }
+            
+            if(creds.getTries() >= 3)
+            {
+                viewmodel.displayExhaustedTries();
+                viewmodel.dispose();
+            }
+                
+        }
+    };
 }
