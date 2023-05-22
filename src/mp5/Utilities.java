3ccbb5a3211c66b1a29a79ca04e24df8f3c71867
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2023-05-23 02:39:06 +0800

    Create Utilities.java
    helper method for retrieving credentials in text

---
+++ src/mp5/Utilities.java
@@ -0,0 +1,59 @@
+package mp5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Utilities 
+{
+    public static HashMap<String, String> getLoginCredentials(String address)
+    {
+        File file = new File(address);
+
+        if(!file.exists())
+        {
+            System.err.println("GETTING LOGIN CREDENTIALS ERROR: File / Directory does not exist.");
+            System.exit(1);
+        }
+
+        BufferedReader reader;
+        ArrayList<String> tempBuffer = new ArrayList<>();
+
+        try
+        {
+            reader = new BufferedReader(new FileReader(file));
+            while(true)
+            {
+                String buffer = reader.readLine();
+                if(buffer == null)
+                    break;
+                
+                tempBuffer.add(buffer);            
+            }
+            reader.close();
+        }
+        catch(FileNotFoundException fnf)
+        {
+            System.err.println("File not found.");
+            System.exit(1);
+        }
+        catch(IOException io)
+        {
+            System.err.println("IOException has occured.");
+            System.exit(1);
+        }
+
+        HashMap<String, String> uMap = new HashMap<>();
+        for(int i = 0; i < tempBuffer.size(); ++i)
+        {
+            if(i % 2 == 0)
+                uMap.put(tempBuffer.get(i), tempBuffer.get(i+1));
+        }
+        
+        return uMap;
+    }
+}
