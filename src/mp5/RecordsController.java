3c63026b7bed95e29e3b20fb390afc597e88f0f8
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2023-05-23 15:41:03 +0800

    Update RecordsController.java

--- src/mp5/RecordsController.java
+++ src/mp5/RecordsController.java
@@ -1,7 +1,30 @@
 package mp5;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
 public class RecordsController 
 {
-    RecordsView viewmodel;
+    CentralRecordsView viewmodel;
+    Person personRecord;
     
+    //viewmodel for addrecord and removerecord
+    
+    public RecordsController(CentralRecordsView viewmodel, Person personRecord)
+    {
+        this.viewmodel = viewmodel;
+        this.personRecord = personRecord;
+    }
+    
+    MouseAdapter onClickEvent = new MouseAdapter() 
+    {
+        @Override
+        public void mouseClicked(MouseEvent e) 
+        {
+            if(!(e.getComponent() instanceof JButton)) return;
+            
+        }
+        
+    };
 }
