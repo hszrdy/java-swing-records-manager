8bf11f380dce0a426b3dded2c656025e96fa99ef
hrkrdy <63145494+hszrdy@users.noreply.github.com>
2023-05-23 21:10:58 +0800

    Update RecordsModifierViewBase.java
    base of add and remove view

--- src/mp5/RecordsModifierViewBase.java
+++ src/mp5/RecordsModifierViewBase.java
@@ -1,5 +1,7 @@
 package mp5;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.HeadlessException;
 import java.awt.event.MouseAdapter;
 import javax.swing.*;
@@ -12,6 +14,8 @@ public class RecordsModifierViewBase extends JFrame
     protected JComboBox<String> monthsBox;
     protected JComboBox<String> daysBox;
     protected JComboBox<String> yearsBox;
+    protected JPanel fieldPanel, btnPanel, datePanel;
+    
 
     public RecordsModifierViewBase()
     {
@@ -24,22 +28,59 @@ public class RecordsModifierViewBase extends JFrame
         actionGoBtn = new JButton();
         actionMoreBtn = new JButton();
         backBtn = new JButton();
+        monthsBox = new JComboBox<>();
+        daysBox = new JComboBox<>();
+        yearsBox = new JComboBox<>();
+        fieldPanel = new JPanel();
+        btnPanel = new JPanel();
+        datePanel = new JPanel();
+        
+        nameLabel.setText("Name: ");
+        backBtn.setText("Back");
+        
+        frameInit(600, 200);
+    }
+    
+    public void frameInit(int x, int y)
+    {
+        this.setResizable(false);
+        this.setSize(x, y);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
     
     public void init()
     {
+        fieldPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        nameField.setPreferredSize(new Dimension(300, 30));
         
+        fieldPanel.add(nameLabel);
+        fieldPanel.add(nameField);
+        
+        btnPanel.add(actionGoBtn);
+        btnPanel.add(actionMoreBtn);
+        btnPanel.add(backBtn);
+        
+        datePanel.add(monthsBox);
+        datePanel.add(daysBox);
+        datePanel.add(yearsBox);
+        
+        this.add(fieldPanel);
+        this.add(datePanel);
+        this.add(btnPanel);
     }
-    
-    public void setVisibility(boolean state)
-    {
-        this.setVisible(state);
-    }
-    
-    public void addBtnListener(MouseAdapter onClickEvent)
+
+    public void addActionGoBtnListener(MouseAdapter onClickEvent)
     {
         actionGoBtn.addMouseListener(onClickEvent);
+    }
+    
+    public void addActionMoreBtnListener(MouseAdapter onClickEvent)
+    {
         actionMoreBtn.addMouseListener(onClickEvent);
+    }
+    
+    public void addBackBtnListener(MouseAdapter onClickEvent)
+    {
         backBtn.addMouseListener(onClickEvent);
     }
 }
